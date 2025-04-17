38546b15c89a4d961ee0b1c564019274b1f7f4f3
mothnue <dinocruz434@gmail.com>
2025-04-17 21:47:54 +0100

    Using stateful widgets

---
+++ lib/screens/settings_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => SettingsScreenState();
+}
+
+class SettingsScreenState extends State<SettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( // Corrigido aqui
+        title: const Text('Settings'),
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
